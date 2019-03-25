af028f6143af44aa7af17beaa91a41234f1936a6
Andrey Metelsky <48751654+vandrusha@users.noreply.github.com>
2019-03-25 21:30:53 +0300

    Create Challenge_8_reverse.js

---
+++ Challenges/Challenge_8_reverse.js
@@ -0,0 +1,33 @@
+// reverse.js
+// Реализуйте и экспортируйте функцию по умолчанию, которая переворачивает строку задом наперед, 
+//используя рекурсию.
+
+//   Например:
+
+// import reverse from './reverse';
+
+// reverse('str');    // rts
+// reverse('hexlet'); // telxeh
+// Попробуйте решить эту задачу, используя рекурсивный процесс.Для этого вам понадобится метод substr.
+
+//   Подсказки
+// Чтобы узнать длину строки, используйте свойство length:
+
+// 'welcome'.length; // 7
+// Чтобы получить подстроку из строки, используйте метод substr:
+
+// 'foo'.substr(1, 2); // 'oo';
+
+const reverse = (str) => {
+const num = str.length - 1
+
+  const rec = (num) => {
+    if (num === 0) {
+      return str[0]
+    }
+    else {
+      return str[num] + rec(num - 1)
+    }
+  }
+  return rec(num);
+}
